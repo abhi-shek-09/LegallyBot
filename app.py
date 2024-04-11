a5ba5aefe37acbb7b8d397e17b5fd2a584930155
adigulalkari <adigulalkari825@gmail.com>
2024-04-11 12:42:17 +0530

    Working application

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import google.generativeai as genai
+from load_creds import load_creds
+
+creds = load_creds()
+
+genai.configure(credentials=creds)
+
+name = "legallybot-nmz2rvthhb9b"
+model = genai.GenerativeModel(model_name=f'tunedModels/{name}')
 
 st.title("LegallyBot ⚖️")
 if "messages" not in st.session_state:
@@ -11,8 +20,16 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What is up?"):
     st.chat_message("user").markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"Echo: {prompt}"
+    result =  model.generate_content(f'{prompt}')
+    response = f"{result.text}"
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+
+
+
+
+# result = model.generate_content('When can the Enrollment Application can be obtained from Bar Council?')
+# print(result.text)
